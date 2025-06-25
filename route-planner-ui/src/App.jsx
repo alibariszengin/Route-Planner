4afe6168c22cbdb21b91d1b18bab0cbf1d8fae39
alibariszengin <alibariszengin@gmail.com>
2025-06-25 07:26:14 +0300

    UI - Add router and navigate screen with SideBar

--- route-planner-ui/src/App.jsx
+++ route-planner-ui/src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LocationsPage from "./pages/Location.jsx";
-import Transportations from "./pages/Transportations.jsx";
+import TransportationsPage from "./pages/Transportations.jsx";
+import Sidebar from "./components/SideBar/SideBar.jsx";
+import {Navigate, Route, BrowserRouter as Router, Routes} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-      <><LocationsPage/>
-      <Transportations/></>
-  )
+    return (
+        <Router>
+            <div className="app-layout" style={{ display: 'flex' }}>
+                <Sidebar />
+                <div className="page-content" style={{ flex: 1, padding: '20px' }}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/locations" />} />
+                        <Route path="/locations" element={<LocationsPage />} />
+                        <Route path="/transportations" element={<TransportationsPage />} />
+                        {/*<Route path="/routes" element={<RoutesPage />} />*/}
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App
